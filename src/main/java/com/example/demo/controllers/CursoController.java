6059574a1a57d916a01ec2ac0bd49114f754c9ff
igorosberg <igorosbergster@gmail.com>
2023-10-06 10:14:51 -0300

    adicionando post put e delete de curso

--- src/main/java/com/example/demo/controllers/CursoController.java
+++ src/main/java/com/example/demo/controllers/CursoController.java
@@ -1,14 +1,17 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Curso;
 import com.example.demo.services.CursoService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 @RequestMapping("/curso")
@@ -18,8 +21,23 @@ public class CursoController {
     CursoService cursoService;
     
     @GetMapping("/{idCurso}")
-    public Curso getCurso(@PathVariable Integer idCurso) throws JsonProcessingException {
+    public Curso getCurso(@PathVariable Integer idCurso) {
         Curso curso = cursoService.getCurso(idCurso);
         return curso;
+    }
+
+    @PostMapping
+    public void cadastrarCurso(@RequestBody Curso curso) {
+        cursoService.salvar(curso);
+    }
+
+    @PutMapping
+    public void atualizarCurso(@RequestBody Curso curso) {
+        cursoService.atualizar(curso);
+    }
+
+    @DeleteMapping("/{idCurso}")
+    public void removerCurso(@PathVariable Integer idCurso) {
+        cursoService.remover(idCurso);
     }
 }
